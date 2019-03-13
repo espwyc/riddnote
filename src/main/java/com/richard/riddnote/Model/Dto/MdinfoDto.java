1bdff1f4ef71401a1f87a0dc35c2c236665cbfc0
richard <q314941534@gmail.com>
2019-03-13 09:09:35 +0800

    modify import

--- src/main/java/com/richard/riddnote/Model/Dto/MdinfoDto.java
+++ src/main/java/com/richard/riddnote/Model/Dto/MdinfoDto.java
@@ -4,7 +4,7 @@ import com.richard.riddnote.Model.Bo.MarkdownBo;
 
 import java.util.Date;
 
-import static com.sun.tools.javac.util.Constants.format;
+//import static com.sun.tools.javac.util.Constants.format;
 
 public class MdinfoDto {
 
